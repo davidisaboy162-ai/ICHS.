2caf9ca7d7f768d0b8c86691935d3b8181c1fbd1
Contractor-x <dada4ash@gmail.com>
2026-04-16 03:01:58 +0100

    added serializers.py

---
+++ backend/apps/users/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+User = get_user_model()
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "password",
+            "phone_number",
+            "role",
+            "latitude",
+            "longitude",
+        ]
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
